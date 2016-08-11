9f951fb4866efea71190114c100e00e2e75ccac6
Pavol Noha <pavol.noha@gmail.com>
2016-08-11 11:25:55 +0200

    Add router, refactoring, redirect handler

---
+++ handlers.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+  "os"
+  "log"
+  "fmt"
+  "strings"
+  "net/http"
+  "io/ioutil"
+  "github.com/gorilla/mux"
+)
+
+func Info(w http.ResponseWriter, r *http.Request) {
+    hostname, _ := os.Hostname()
+    fmt.Fprintf(os.Stdout, "I'm %s\n", hostname)
+    fmt.Fprintf(w, "I'm %s\n", hostname)
+}
+
+func Redirect(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    service := vars["service"]
+    url := []string{"http://", service, ":", os.Getenv("PORT")}
+
+    response, err := http.Get(strings.Join(url, ""))
+    if err != nil {
+    	log.Fatal(err)
+    } else {
+    	defer response.Body.Close()
+        contents, err := ioutil.ReadAll(response.Body)
+        if err != nil {
+            log.Fatal(err)
+        }        
+        fmt.Fprintf(os.Stdout, "Redirect to [%s] service. Response: %s\n", service, string(contents))
+        fmt.Fprintf(w, "Redirect to [%s] service. Response: %s\n", service, string(contents))
+    }
+}
--- main.go
+++ main.go
@@ -1,24 +1,15 @@
 package main
 
 import (
-  "os"
-  "fmt"
-  "net/http"
-  "log"
+    "os"
+    "fmt"
+    "log"
+    "net/http"
 )
-
-func getInfo(w http.ResponseWriter, r *http.Request) {
-    hostname, _ := os.Hostname()
-    fmt.Fprintf(os.Stdout, "I'm %s\n", hostname)
-    fmt.Fprintf(w, "I'm %s\n", hostname)
-}
 
 func main() {
     port := os.Getenv("PORT")
+    router := NewRouter()
     fmt.Fprintf(os.Stdout, "Listening on port [%s]\n", port)
-    http.HandleFunc("/", getInfo)
-    err := http.ListenAndServe(":" + port, nil)
-    if err != nil {
-        log.Fatal("ListenAndServe: ", err)
-    }
+    log.Fatal(http.ListenAndServe(":" + port, router))
 }
